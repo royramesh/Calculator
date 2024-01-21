23b8b642ac71af7842b04eda0220870b5aff1d8b
rameshroy <rameshroyprl2019@gmail.com>
2024-01-21 12:59:47 +0530

    commit msg

---
+++ script.js
@@ -0,0 +1,20 @@
+let str = "";
+let btn = document.querySelectorAll('.btn');
+Array.from(btn).forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            str = eval(str);
+            document.querySelector('.input').value = str;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            str = "";
+            document.querySelector('.input').value = str;
+        }
+        else{
+            console.log(e.target);
+            str = str + e.target.innerHTML;
+            document.querySelector('.input').value = str;
+        }
+        
+    })
+})
